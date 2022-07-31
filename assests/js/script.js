88fc9a1a589c82b772c5b379bb4fb8ebec322111
ibtasam116 <ibtasam116@gmail.com>
2022-07-31 13:47:55 +0500

    Modal Section Complete Time:01/46 | Date: Sunday, 7/31/2022

--- assests/js/script.js
+++ assests/js/script.js
@@ -31,11 +31,11 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /* ======================== ACCORDTION SKILLS ========================  */
 const skillsContent = document.getElementsByClassName('skills_content'),
-skillsHeader = document.querySelectorAll('.skills_header')
+    skillsHeader = document.querySelectorAll('.skills_header')
 
 function toggleSkills() {
     let itemClass = this.parentNode.className
-    
+
     for (i = 0; i < skillsContent.length; i++) {
         skillsContent[i].className = 'skills_content skills_close'
     }
@@ -44,7 +44,7 @@ function toggleSkills() {
     }
 }
 
-skillsHeader.forEach((el) =>{
+skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
@@ -52,22 +52,57 @@ skillsHeader.forEach((el) =>{
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]')
 
-    tabs.forEach(tab =>{
-        tab.addEventListener('click', () => {
-            const target = document.querySelector(tab.dataset.target)
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
 
-            tabContents.forEach(tabContent =>{
-                tabContent.classList.remove('qualification_active')
-            })
-            target.classList.add('qualification_active')
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
 
-            tab.forEach(tab =>{
-                tab.classList.remove('qualification_active')
-            })
-            tab.classList.add('qualification_active')
+        tab.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
+/* ======================== SERVICES MODAL ========================  */
+const modalViews = document.querySelectorAll('.services_modal'),
+    modalBtns = document.querySelectorAll('.services_button'),
+    modalCloses = document.querySelectorAll('.services_modal-close')
+
+let modal = function (modalClick) {
+    modalViews[modalClick].classList.add('active-modal')
+}
+
+modalBtns.forEach((modalBtn, i) => {
+    modalBtn.addEventListener('click', () => {
+        modal(i)
+    })
+})
+
+modalCloses.forEach((modalClose) => {
+    modalClose.addEventListener('click', () => {
+        modalViews.forEach((modalView) => {
+            modalView.classList.remove('active-modal')
         })
     })
+})
 
+
+
+
+
+
+
+
+/* ======================== QUALIFICATION TABS ========================  */
+/* ======================== QUALIFICATION TABS ========================  */
+/* ======================== QUALIFICATION TABS ========================  */
+/* ======================== QUALIFICATION TABS ========================  */
+/* ======================== QUALIFICATION TABS ========================  */
 
 
 
