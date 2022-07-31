9bfd1fa579eb148ba4c7585e86daac69b603b947
ibtasam116 <ibtasam116@gmail.com>
2022-07-31 22:19:20 +0500

    Dark/Light Mode Complete Time: 10:18 PM Date: Sunday, 7/31/2022

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,21 +3,25 @@ const navMenu = document.getElementById('nav-menu'),
     navToggle = document.getElementById('nav-toggle'),
     navClose = document.getElementById('nav-close')
 
+
 /* ============= MENU SHOW ============= */
-/* validate if constant exists */
+/* ===== validate if constant exists ===== */
 if (navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
+
 /* ============= MENU HIDDEN ============= */
-/* validate if constant exists */
+/* ===== validate if constant exists ===== */
 if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
     })
 }
+
+
 
 /* ======================== REMOVE MENU MOBILE ========================  */
 const navLink = document.querySelectorAll('.nav_link')
@@ -28,6 +32,8 @@ function linkAction() {
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
 
 /* ======================== ACCORDTION SKILLS ========================  */
 const skillsContent = document.getElementsByClassName('skills_content'),
@@ -48,6 +54,8 @@ skillsHeader.forEach((el) => {
     el.addEventListener('click', toggleSkills)
 })
 
+
+
 /* ======================== QUALIFICATION TABS ========================  */
 const tabs = document.querySelectorAll('[data-target]'),
     tabContents = document.querySelectorAll('[data-content]')
@@ -67,6 +75,8 @@ tabs.forEach(tab => {
         tab.classList.add('qualification_active')
     })
 })
+
+
 
 /* ======================== SERVICES MODAL ========================  */
 const modalViews = document.querySelectorAll('.services_modal'),
@@ -91,6 +101,8 @@ modalCloses.forEach((modalClose) => {
     })
 })
 
+
+
 /* ======================== PORTFOLIO SWIPER ========================  */
 let swiperPortfolio = new Swiper(".portfolio_container", {
     cssMode: true,
@@ -105,6 +117,8 @@ let swiperPortfolio = new Swiper(".portfolio_container", {
         clickable: true,
     },
 });
+
+
 
 /* ======================== TESTIMONIAL ========================  */
 let swiperTestimonial = new Swiper(".testimonial_container", {
@@ -126,9 +140,80 @@ let swiperTestimonial = new Swiper(".testimonial_container", {
 
 
 
+/* ==================== SCROLL SECTIONS ACTIVE LINK ==================== */
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
-/* ======================== QUALIFICATION TABS ========================  */
+
+/* ==================== CHANGE BACKGROUND HEADER ==================== */
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
+
+
+/*==================== SHOW SCROLL UP ====================*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    if (this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+
+
+/* ======================== DARK LIGHT THEME ========================  */
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+
+
 /* ======================== QUALIFICATION TABS ========================  */
 /* ======================== QUALIFICATION TABS ========================  */
 
